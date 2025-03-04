167b95524e4e8a75ed9a774266fb22b04729e1ae
Gianluca Turra <gianluca.turra@sanmarcoinformatica.it>
2025-03-04 18:01:30 +0100

    Introducing libraries

--- main.go
+++ main.go
@@ -4,13 +4,50 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+type model struct {
+	output string
+}
+
+type resultMsg string
+
 func main() {
-	branches := strings.Split(executeShellScript("./getAllBranches.sh"), "\n")
-	fmt.Println(filterStringSlice(branches))
+	p := tea.NewProgram(model{})
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running Bubble Tea program:", err)
+		os.Exit(1)
+	}
+	// branches := strings.Split(executeShellScript("./getAllBranches.sh"), "\n")
+	// fmt.Println(filterStringSlice(branches))
+	// cmd.Execute()
+}
+
+func (m model) Init() tea.Cmd {
+	return runShellScript
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case resultMsg:
+		m.output = string(msg)
+		return m, nil
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("Command Output:\n\n%s\n\n[Press q to quit]", m.output)
+}
+
+func runShellScript() tea.Msg {
+	return resultMsg(executeShellScript("./getAllBranches.sh"))
 }
 
 func executeShellScript(scriptName string) string {
